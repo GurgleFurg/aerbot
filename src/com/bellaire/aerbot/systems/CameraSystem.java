5b5930deba9f3b9970ca80ea90dfac647699dba8
LoadingPleaseWait <anonymous9304@gmail.com>
2014-01-15 18:00:52 -0600

    Update CameraSystem.java

--- src/com/bellaire/aerbot/systems/CameraSystem.java
+++ src/com/bellaire/aerbot/systems/CameraSystem.java
@@ -10,14 +10,35 @@ package com.bellaire.aerbot.systems;
  *
  * @author Agro
  */
+
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.image.NIVisionException;
+
+
 public class CameraSystem implements RobotSystem {
 
+    private AxisCamera camera;
+    
     public void init() {
-        
+        camera = AxisCamera.getInstance();
     }
 
     public void destroy() {
         
     }
     
+    public ColorImage getImage(){
+        ColorImage image = null;
+        try{
+            image = camera.getImage();
+        }catch (AxisCameraException ex){
+            
+        }catch (NIVisionException ex){
+            
+        }
+        return image;
+    }
 }
+
